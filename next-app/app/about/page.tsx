c80c8eabb6806fadc268bb2ef8a76573c3d71503
Lee Williams <leemw1977@gmail.com>
2025-01-21 15:53:10 +0000

    Initialise next app

---
+++ next-app/app/about/page.tsx
@@ -0,0 +1,10 @@
+function About() {
+    return (
+        <div>
+            <h2>About Us</h2>
+            <p>A small sentence in a paragraph element.</p>
+        </div>
+    );
+}
+
+export default About;
